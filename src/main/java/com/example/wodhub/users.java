b5cbf4ff8f34bf4ded2e5bd052ae4b56ec339b65
Michael <Michael.evans1775@outlook.com>
2023-03-06 14:18:29 -0700

    add user Class

---
+++ src/main/java/com/example/wodhub/users.java
@@ -0,0 +1,49 @@
+package com.example.wodhub;
+
+public class users {
+    private String role;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String passWord;
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
