7a25061688d5ed98eb862b7f72267670ba55735d
Michael <Michael.evans1775@outlook.com>
2023-03-06 12:00:16 -0700

    test

--- src/main/java/com/example/wodhub/Login_Controller.java
+++ src/main/java/com/example/wodhub/Login_Controller.java
@@ -3,7 +3,7 @@ package com.example.wodhub;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Login_Controller {
     @FXML
     private Label welcomeText;
 
